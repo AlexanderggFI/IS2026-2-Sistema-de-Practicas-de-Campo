49f8c7c5731bbe5aa1ed9f5314030b1249de73e4
Adahirsito <adahir.hm@gmail.com>
2026-05-30 14:47:46 -0600

    Validacion Contraseña registro y detalles esteticos

--- js/script_home.js
+++ js/script_home.js
@@ -17,15 +17,4 @@ botonMenu.addEventListener('click', () => {
         localStorage.setItem('menuDesplegado', 'false');
     }
 });
-/*
-const botonMenu = document.getElementById('btnBarras');
-const pantalla = document.getElementById('pantallaBienvenida');
 
-document.getElementById("nombrePerfil").innerHTML = usuario;
-
-botonMenu.addEventListener('click', () => {
-    pantalla.classList.toggle('mostrar');
-    flag = true;
-    
-});
-*/
